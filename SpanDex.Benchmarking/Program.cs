db8c3899a6f667430dfff07b8527c6b6d9188ff4
Jono Rogers <jono@statusas.com>
2019-07-28 18:19:11 +0100

    Added some benchmarking info for interests sake

---
+++ SpanDex.Benchmarking/Program.cs
@@ -0,0 +1,49 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+
+namespace SpanDex.Benchmarking {
+    [RankColumn]
+    [MemoryDiagnoser]
+    public class SpanReaderVsBitConverter {
+        private byte[] data;
+
+        [Params(1000, 10000)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup() {
+            data = new byte[N * sizeof(Int16)];
+        }
+
+        [Benchmark]
+        public void SpanReader() {
+            SpanReader reader = data;
+
+            for (int i = 0; i < N; i += 2) {
+                var v = reader.ReadInt16LittleEndian();
+            }
+        }
+
+        [Benchmark]
+        public void BitConvert() {
+            for (int i = 0; i < N; i += 2) {
+                var v = BitConverter.ToInt16(data, i);
+            }
+        }
+
+        [Benchmark]
+        public void Manual() {
+            for (int i = 0; i < N; i += 2) {
+                var v = (short)((data[i] << 8) | data[i + 1]);
+            }
+        }
+    }
+
+    public class Program {
+        public static void Main() {
+            var summary = BenchmarkRunner.Run<SpanReaderVsBitConverter>();
+        }
+    }
+}
